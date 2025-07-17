96064fc635c9514ca9b5d9e0f409f9604d453ba8
likshan-lahiru <lahiru212001@gmail.com>
2025-07-18 02:22:16 +0530

    feat: improve StatusBar component with enhanced mobile and desktop layouts

--- src/components/StatusBar.tsx
+++ src/components/StatusBar.tsx
@@ -1,66 +1,77 @@
 import React from 'react'
 import { BalanceSelector } from './BalanceSelector'
+
 export function StatusBar({ isMobile }: { isMobile?: boolean }) {
     return (
         <div className="p-2 sm:p-4 flex flex-col sm:flex-row sm:justify-between sm:items-center">
-            {/* Mobile view remains unchanged */}
             {isMobile ? (
                 <>
-                    {/* Balance Selector - Full width on mobile */}
+                    {/* Mobile Balance Selector */}
                     <div className="w-full mb-2">
                         <BalanceSelector
                             onSelect={(type) => console.log(`Selected: ${type}`)}
                         />
                     </div>
-                    {/* Heart and Diamond - Row on mobile */}
+
+                    {/* Mobile Heart & Diamond */}
                     <div className="flex justify-center space-x-3 mb-2">
                         <div className="w-36 h-10 bg-[#111827] rounded-full flex items-center px-4">
-                            <img
-                                src="https://uploadthingy.s3.us-west-1.amazonaws.com/agrcZVSRX593jbti3xzVTM/heart.png"
-                                alt="Heart"
-                                className="w-7 h-7"
-                            />
+                            <div className="w-8 h-8 flex items-center justify-center">
+                                <img
+                                    src="https://uploadthingy.s3.us-west-1.amazonaws.com/agrcZVSRX593jbti3xzVTM/heart.png"
+                                    alt="Heart"
+                                    className="w-full h-full object-contain"
+                                />
+                            </div>
                             <span className="ml-1">0</span>
                         </div>
                         <div className="w-36 h-10 bg-[#111827] rounded-full flex items-center px-4">
-                            <img
-                                src="https://uploadthingy.s3.us-west-1.amazonaws.com/uwPYNNRiavmZZ285SkzD5Z/diaomnd.png"
-                                alt="Diamond"
-                                className="w-8 h-8"
-                            />
+                            <div className="w-8 h-8 flex items-center justify-center">
+                                <img
+                                    src="https://uploadthingy.s3.us-west-1.amazonaws.com/uwPYNNRiavmZZ285SkzD5Z/diaomnd.png"
+                                    alt="Diamond"
+                                    className="w-full h-full object-contain"
+                                />
+                            </div>
                             <span className="ml-1">0</span>
                         </div>
                     </div>
-                </> /* Desktop view - all elements on one line */
+                </>
             ) : (
                 <>
-                    {/* Online count - now inline instead of absolute */}
+                    {/* Desktop Online Count */}
                     <div className="flex items-center space-x-3">
                         <div className="w-3 h-3 rounded-full bg-green-500"></div>
                         <span className="text-sm">1,568 Online</span>
                     </div>
-                    {/* Balance Selector */}
+
+                    {/* Desktop Balance Selector */}
                     <div className="flex-1 max-w-md mx-auto px-4">
                         <BalanceSelector
                             onSelect={(type) => console.log(`Selected: ${type}`)}
                         />
                     </div>
-                    {/* Heart and Diamond - Keep in a column on desktop */}
+
+                    {/* Desktop Heart & Diamond */}
                     <div className="flex flex-col space-y-1">
                         <div className="w-48 h-12 bg-[#111827] rounded-full flex items-center px-6">
-                            <img
-                                src="https://uploadthingy.s3.us-west-1.amazonaws.com/agrcZVSRX593jbti3xzVTM/heart.png"
-                                alt="Heart"
-                                className="w-9 h-9"
-                            />
+                            <div className="w-7 h-7 flex items-center justify-center">
+                                <img
+                                    src="https://uploadthingy.s3.us-west-1.amazonaws.com/agrcZVSRX593jbti3xzVTM/heart.png"
+                                    alt="Heart"
+                                    className="w-14 h-28 object-contain"
+                                />
+                            </div>
                             <span className="ml-1">0</span>
                         </div>
                         <div className="w-48 h-12 bg-[#111827] rounded-full flex items-center px-6">
-                            <img
-                                src="https://uploadthingy.s3.us-west-1.amazonaws.com/uwPYNNRiavmZZ285SkzD5Z/diaomnd.png"
-                                alt="Diamond"
-                                className="w-10 h-10"
-                            />
+                            <div className="w-8 h-8 flex items-center justify-center">
+                                <img
+                                    src="https://uploadthingy.s3.us-west-1.amazonaws.com/uwPYNNRiavmZZ285SkzD5Z/diaomnd.png"
+                                    alt="Diamond"
+                                    className="w-14 h-28 object-contain"
+                                />
+                            </div>
                             <span className="ml-1">0</span>
                         </div>
                     </div>
