aa6d9c914a1ce1605728dbd99c412f7eb4d71ef9
LRLayan <rameshlayan4@gmail.com>
2025-07-30 15:40:25 +0530

    modal lg and add padding

--- src/components/ForgotPasswordModal.tsx
+++ src/components/ForgotPasswordModal.tsx
@@ -43,7 +43,7 @@ export function ForgotPasswordModal({
   if (!isOpen) return null
   return (
       <div className="fixed inset-0 z-50 flex items-center justify-center bg-[#1F2937E5]/90">
-        <div className="bg-[#374151] font-['Inter']  rounded-lg p-5 sm:p-8 shadow-xl w-full max-w-md mx-4">
+        <div className="bg-[#374151] font-['Inter'] rounded-lg p-10 shadow-xl w-full max-w-lg mx-4">
           <h2 className="text-white text-lg sm:text-xl font-bold text-center mb-3 sm:mb-4">
             Password Reset
           </h2>
@@ -65,19 +65,21 @@ export function ForgotPasswordModal({
                 />
             ))}
           </div>
-          <button
-              className=" w-72 ml-10 bg-[#2D7FF0] hover:bg-blue-600 text-white font-bold py-2 sm:py-3 px-4 rounded-2xl transition-colors mb-2 sm:mb-3 text-sm sm:text-base"
-              onClick={handleVerifyCode}
-          >
-            Verify
-          </button>
-          <button
-              type="button"
-              className="w-full text-center  hover:underline py-2 text-xs sm:text-sm font-['Inter']"
-              onClick={handleResendCode}
-          >
-            Resend code
-          </button>
+          <div className={"flex flex-col items-center"}>
+            <button
+                className=" w-72 bg-[#2D7FF0] hover:bg-blue-600 text-white font-bold py-2 sm:py-3 px-4 rounded-2xl transition-colors mb-2 sm:mb-3 text-sm sm:text-base"
+                onClick={handleVerifyCode}
+            >
+              Verify
+            </button>
+            <button
+                type="button"
+                className="w-full text-center hover:underline py-2 text-xs sm:text-sm font-['Inter']"
+                onClick={handleResendCode}
+            >
+              Resend code
+            </button>
+          </div>
         </div>
       </div>
   )
