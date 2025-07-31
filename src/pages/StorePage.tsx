d15e4ed5a0c131ac5b156eaaabd46b91fe0430a3
LRLayan <rameshlayan4@gmail.com>
2025-07-31 11:44:42 +0530

    store page issues fix

--- src/pages/StorePage.tsx
+++ src/pages/StorePage.tsx
@@ -800,7 +800,7 @@ export function StorePage() {
         <div className="flex flex-1 pl-16 pr-5 pb-8">
 
           {/* Left sidebar */}
-          <div className={`${activeTabDesktop === 'redeem' ? 'mt-16 w-[440px]' : 'w-72'} bg-[#374151] rounded-xl p-6 mr-4`}>
+          <div className={`${activeTabDesktop === 'redeem' ? 'mt-16 w-[467px]' : 'w-72'} bg-[#374151] rounded-xl p-6 mr-4`}>
             <h1 className="text-2xl font-bold mb-8">Store</h1>
             <button
                 className={`${activeTabDesktop === 'coins' ? 'bg-blue-500 hover:bg-blue-600' : ' bg-[#1F2937] hover:bg-[#0A0E1A]'} w-full text-white py-4 px-5 rounded-xl mb-4 flex items-center`}
@@ -831,7 +831,7 @@ export function StorePage() {
                 <img
                     src="https://uploadthingy.s3.us-west-1.amazonaws.com/5ARgETPVNopfYddtEfN6Yn/redeem.png"
                     alt="Redeem"
-                    className="w-10 h-10"
+                    className="w-12 h-12"
                 />
               </div>
             </button>
@@ -843,7 +843,7 @@ export function StorePage() {
               <>
                 {/* Right content area */}
                 <div className={`flex-1 bg-[#374151] rounded-xl p-6 `}>
-                  <h2 className="text font-medium mb-6">GC Package</h2>
+                  <h2 className="text font-medium mb-6 font-['DM Sans']">GC Package</h2>
 
                   {/* Package cards - updated to be responsive with min/max width */}
                   <div className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-y-16 gap-4 mb-8`}>
@@ -882,7 +882,7 @@ export function StorePage() {
                             {/* Package content - with consistent alignment */}
                             <div className="flex-1 flex flex-col text-center px-4">
                               {/* GC amount */}
-                              <p className="font-bold text-xl text-black mb-2">
+                              <p className="font-bold text-xl text-black mb-2 font-['DM Sans']">
                                 GC {pkg.coins.toLocaleString()}
                               </p>
 
@@ -891,14 +891,14 @@ export function StorePage() {
                                 {
                                   pkg.bonus > 0 ? (
                                       <>
-                                        <p className="text-black">+</p>
+                                        <p className="text-black text-xl">+</p>
                                         <div className="flex items-center justify-center mt-1">
                                           <img
                                               src="https://uploadthingy.s3.us-west-1.amazonaws.com/mmaJ4fycdupGhSyQnVgCcX/Entries.png"
                                               alt="Ticket"
-                                              className="w-6 h-6 mr-1"
+                                              className="w-8 h-8 mr-1 bg-[#0CC242] rounded-full p-[2px]"
                                           />
-                                          <span className="text-black font-bold">
+                                          <span className="text-black text-xl font-bold font-['DM Sans']">
                                             × {pkg.bonus} free
                                           </span>
                                         </div>
@@ -909,13 +909,14 @@ export function StorePage() {
                                 }
                               </div>
 
-                              {/* Price */}
-                              <p className="font-bold text-xl text-black my-4">
-                                ${pkg.price}
-                              </p>
-
                               {/* Buy button - always at the same position */}
                               <div className="mt-auto mb-8 px-4">
+
+                                {/* Price */}
+                                <p className="font-bold text-xl text-black my-4 font-['DM Sans']">
+                                  ${pkg.price}
+                                </p>
+
                                 <button
                                     onClick={() => handleBuy(pkg)}
                                     className="w-full bg-[#56CA5A] hover:bg-[#4BB850] text-white py-3 rounded-full font-bold"
