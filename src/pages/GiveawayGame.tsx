f97cc700c92f039237d1a7eb1d7f06fc6936746d
LRLayan <rameshlayan4@gmail.com>
2025-08-01 18:15:42 +0530

    Entries path design

--- src/pages/GiveawayGame.tsx
+++ src/pages/GiveawayGame.tsx
@@ -3,17 +3,18 @@ import { useNavigate, useLocation } from 'react-router-dom'
 import { BottomNavigation } from '../components/BottomNavigation'
 import { StatusBar } from '../components/StatusBar'
 import { useGlobalContext } from '../context/GlobalContext'
-import { PrizeCard, PrizeData } from '../components/PrizeCard'
+import { PrizeCard, PrizeData} from '../components/PrizeCard'
+
 export function GiveawayGame() {
   const navigate = useNavigate()
   const location = useLocation()
-  const { spinBalance, coinBalance, setCoinBalance } = useGlobalContext()
+  const { coinBalance, setCoinBalance, selectedBalanceType, ticketBalance, voucherBalance, setVoucherBalance, setTicketBalance } = useGlobalContext()
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768)
   // Get the selected game from location state
-  const { selectedGame = 'wordoll' } =
-  (location.state as {
+  const { selectedGame = 'wordoll' } = (location.state as {
     selectedGame?: string
   }) || {}
+
   useEffect(() => {
     const handleResize = () => {
       setIsMobile(window.innerWidth <= 768)
@@ -21,16 +22,20 @@ export function GiveawayGame() {
     window.addEventListener('resize', handleResize)
     return () => window.removeEventListener('resize', handleResize)
   }, [])
+
   const handleEnterGame = (prize: PrizeData) => {
+
     // Check if user has enough coins
     if (coinBalance < prize.cost) {
       alert("You don't have enough coins to play for this prize!")
       return
     }
+
     // Deduct the cost from user's coin balance
     setCoinBalance(coinBalance - prize.cost)
     // Store the selected prize in session storage
     sessionStorage.setItem('selectedPrize', JSON.stringify(prize))
+
     // Navigate to the appropriate game based on the selected game
     if (selectedGame === 'wordoll') {
       navigate('/giveaway-wordoll-game')
@@ -38,6 +43,22 @@ export function GiveawayGame() {
       navigate('/giveaway-lockpickr-game')
     }
   }
+
+  const handleEnterGameEntries = (entries: PrizeData) => {
+    // Check if user has enough coins
+    if (ticketBalance < entries.cost) {
+      alert("You don't have enough entries to play for this prize!")
+      return
+    }
+
+    setVoucherBalance(voucherBalance + entries.spinAmount)
+
+    // Deduct the cost from user's entries balance
+    setTicketBalance(ticketBalance - entries.cost)
+    // Store the selected prize in session storage
+    sessionStorage.setItem('selectedEntryPrize', JSON.stringify(entries))
+  }
+
   // Prize data array
   const prizes: PrizeData[] = [
     {
@@ -73,6 +94,7 @@ export function GiveawayGame() {
           'https://uploadthingy.s3.us-west-1.amazonaws.com/ofQaY3MrbupDhcPiD5MFSJ/prizez-coins-4.png',
     },
   ]
+
   // For desktop view, use slightly different values
   const desktopPrizes: PrizeData[] = [
     {
@@ -108,6 +130,43 @@ export function GiveawayGame() {
           'https://uploadthingy.s3.us-west-1.amazonaws.com/ofQaY3MrbupDhcPiD5MFSJ/prizez-coins-4.png',
     },
   ]
+
+  // Buy Entries
+  const entries : PrizeData[] = [
+    {
+      id: 1,
+      coinAmount: 50000,
+      spinAmount: 2,
+      cost: 2,
+      image:
+          'https://uploadthingy.s3.us-west-1.amazonaws.com/5sNDV16zKDrZv4WE8wxPR4/prizez-coins-1.png',
+    },
+    {
+      id: 2,
+      coinAmount: 300000,
+      spinAmount: 5,
+      cost: 5,
+      image:
+          'https://uploadthingy.s3.us-west-1.amazonaws.com/xhwr6vr8mJJCTeAgmtWgrD/prizez-coins-2.png',
+    },
+    {
+      id: 3,
+      coinAmount: 1500000,
+      spinAmount: 16,
+      cost: 15,
+      image:
+          'https://uploadthingy.s3.us-west-1.amazonaws.com/6vJnKJ8AUVGEnXgBRiWAH9/prizez-coins-3.png',
+    },
+    {
+      id: 4,
+      coinAmount: 8000000,
+      spinAmount: 75,
+      cost: 73,
+      image:
+          'https://uploadthingy.s3.us-west-1.amazonaws.com/ofQaY3MrbupDhcPiD5MFSJ/prizez-coins-4.png',
+    },
+  ]
+
   if (isMobile) {
     return (
         <div className="flex flex-col w-full min-h-screen bg-[#1F2937] text-white font-['DM_Sans']">
@@ -123,15 +182,18 @@ export function GiveawayGame() {
               />
             </button>
           </div>
+
           {/* Status Bar */}
           <div className="md:pl-52">
             <StatusBar isMobile={isMobile} hideOnlineCount={true} />
           </div>
+
           {/* Main Content */}
           <div className="flex-1 flex flex-col px-3 pt-1">
             <h2 className="text-xl font-medium text-center mb-6">
               Select a prize to win !
             </h2>
+
             {/* Prize Cards */}
             <div className="space-y-1 mb-8">
               {prizes.map((prize) => (
@@ -144,13 +206,17 @@ export function GiveawayGame() {
               ))}
             </div>
           </div>
+
           {/* Bottom Navigation */}
           <BottomNavigation />
         </div>
     )
   }
+
   return (
       <div className="flex flex-col w-full min-h-screen bg-[#1F2937] text-white font-['DM_Sans']">
+
+        {/* Back Button */}
         <div className="absolute top-8 left-6 z-10">
           <button
               className="w-14 h-14 rounded-full flex items-center justify-center"
@@ -163,27 +229,47 @@ export function GiveawayGame() {
             />
           </button>
         </div>
+
         {/* Status Bar */}
         <div className="">
           <StatusBar isMobile={isMobile} hideOnlineCount={true} switchableBalanceSelector={true}/>
         </div>
+
         {/* Main Content */}
         <div className="flex-1 flex flex-col items-center justify-start px-4 pt-12">
           <h2 className="text-xl font-['DM_Sans'] font-medium text-center mr-96 pr-96 mb-8">
-            Select a prize to win!
+            { selectedBalanceType === 'coin' ? 'Select a prize to win!' : 'Select a prize to win! (One-time, single enter only per 24h)                                   '}
           </h2>
+
           {/* Prize Cards Row */}
           <div className="flex flex-wrap justify-center gap-4 w-full max-w-5xl mb-8">
-            {desktopPrizes.map((prize) => (
-                <PrizeCard
-                    key={prize.id}
-                    prize={prize}
-                    isMobile={false}
-                    onEnter={() => handleEnterGame(prize)}
-                />
-            ))}
+            { selectedBalanceType === 'coin' ? (
+                  <>
+                    {desktopPrizes.map((prize) => (
+                        <PrizeCard
+                            key={prize.id}
+                            prize={prize}
+                            isMobile={false}
+                            onEnter={() => handleEnterGame(prize)}
+                        />
+                    ))}
+                  </>
+              ) : (
+                  <>
+                    {entries.map((entries) => (
+                        <PrizeCard
+                            key={entries.id}
+                            prize={entries}
+                            isMobile={false}
+                            onEnter={() => handleEnterGameEntries(entries)}
+                        />
+                    ))}
+                  </>
+              )
+            }
           </div>
         </div>
+
         {/* Bottom Navigation */}
         <BottomNavigation />
       </div>
