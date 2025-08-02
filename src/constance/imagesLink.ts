e6f4e771652cc0676655a43f4f81e9b673e7ecea
LRLayan <rameshlayan4@gmail.com>
2025-08-02 21:50:36 +0530

    add imageLink

--- src/constance/imagesLink.ts
+++ src/constance/imagesLink.ts
@@ -2,4 +2,6 @@ export const IMAGES = {
     voucher: 'https://uploadthingy.s3.us-west-1.amazonaws.com/uwPYNNRiavmZZ285SkzD5Z/diaomnd.png',
     diamond: 'https://uploadthingy.s3.us-west-1.amazonaws.com/agrcZVSRX593jbti3xzVTM/heart.png',
     coin: 'https://uploadthingy.s3.us-west-1.amazonaws.com/2XiBYwBWgNJxytH6Z2jPWP/point.png',
+    fortuneCooky: '/fortuneCooky.png',
+    ticket: 'https://uploadthingy.s3.us-west-1.amazonaws.com/65WCbcmf6dyyeqvjSAJHyp/fire.png',
 };
