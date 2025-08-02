6edfae139a7d50f9a105b3e172e2ba465a39428a
LRLayan <rameshlayan4@gmail.com>
2025-08-02 21:05:28 +0530

    images links

---
+++ src/constance/imagesLink.ts
@@ -0,0 +1,5 @@
+export const IMAGES = {
+    voucher: 'https://uploadthingy.s3.us-west-1.amazonaws.com/uwPYNNRiavmZZ285SkzD5Z/diaomnd.png',
+    diamond: 'https://uploadthingy.s3.us-west-1.amazonaws.com/agrcZVSRX593jbti3xzVTM/heart.png',
+    coin: 'https://uploadthingy.s3.us-west-1.amazonaws.com/2XiBYwBWgNJxytH6Z2jPWP/point.png',
+};
