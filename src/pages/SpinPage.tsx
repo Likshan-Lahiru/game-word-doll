93d02923d0d27a3d5e09b5ecfbafb6714abc21b8
LRLayan <rameshlayan4@gmail.com>
2025-08-03 23:01:47 +0530

    spin page modify

--- src/pages/SpinPage.tsx
+++ src/pages/SpinPage.tsx
@@ -4,10 +4,30 @@ import { StatusBar } from '../components/StatusBar'
 import { BottomNavigation } from '../components/BottomNavigation'
 import { useGlobalContext } from '../context/GlobalContext'
 import { CongratsModal } from '../components/CongratsModal'
+import {IMAGES} from "../constance/imagesLink.ts";
+
+const spinVoucherCountData = [
+    {
+        id: 1,
+        count: 0.20
+    },
+    {
+        id: 2,
+        count: 0.40
+    },
+    {
+        id: 3,
+        count: 1
+    },
+    {
+        id: 4,
+        count: 2
+    }
+]
+
 export function SpinPage() {
     const navigate = useNavigate()
-    const { spinBalance, addSpins, addCoins, coinBalance, gemBalance } =
-        useGlobalContext()
+    const { spinBalance, addSpins, addCoins, coinBalance, gemBalance, selectedBalanceType } = useGlobalContext()
     const [isMobile, setIsMobile] = useState(window.innerWidth <= 768)
     const [isSpinning, setIsSpinning] = useState(false)
     const [rotationDegrees, setRotationDegrees] = useState(0)
@@ -16,7 +36,12 @@ export function SpinPage() {
         coins: 0,
         spins: 0,
     })
+
+    let [spinVoucherId, setSpinVoucherId] = useState(2)
+    const [spinVoucherCount, setSpinVoucherCount] = useState(0.40)
+
     const wheelRef = useRef<HTMLDivElement>(null)
+
     // Prizes configuration
     const prizes = [
         {
@@ -60,6 +85,7 @@ export function SpinPage() {
             spins: 0,
         },
     ]
+
     // Add some spins for testing if none are available
     useEffect(() => {
         if (spinBalance <= 0) {
@@ -68,6 +94,7 @@ export function SpinPage() {
             console.log('Added 5 spins for testing')
         }
     }, [])
+
     useEffect(() => {
         const handleResize = () => {
             setIsMobile(window.innerWidth <= 768)
@@ -75,6 +102,7 @@ export function SpinPage() {
         window.addEventListener('resize', handleResize)
         return () => window.removeEventListener('resize', handleResize)
     }, [])
+
     const spinWheel = () => {
         console.log('Spin button clicked', {
             spinBalance,
@@ -97,23 +125,28 @@ export function SpinPage() {
         const fullRotations = 3 + Math.floor(Math.random() * 3)
         const randomPrizeIndex = Math.floor(Math.random() * 8)
         const segmentAngle = 45 // 360 degrees / 8 segments
+
         // Calculate final rotation (multiple full rotations + position to stop at the random prize)
-        const finalRotation =
-            fullRotations * 360 + (360 - randomPrizeIndex * segmentAngle)
+        const finalRotation = fullRotations * 360 + (360 - randomPrizeIndex * segmentAngle)
+
         // Set the new absolute rotation value
         setRotationDegrees((prevRotation) => prevRotation + finalRotation)
         console.log(
             `Spinning to ${rotationDegrees + finalRotation} degrees (prize index: ${randomPrizeIndex})`,
         )
+
         // After animation completes, show the prize
         setTimeout(() => {
+
             setIsSpinning(false)
             const wonPrize = prizes[randomPrizeIndex]
             setPrize(wonPrize)
+
             // Add the coins to the user's balance
             if (wonPrize.coins > 0) {
                 addCoins(wonPrize.coins)
             }
+
             // Add any spins won
             if (wonPrize.spins > 0) {
                 addSpins(wonPrize.spins)
@@ -121,11 +154,34 @@ export function SpinPage() {
             setShowCongratsModal(true)
         }, 12000) // Match this to the new animation duration (7s + 5s)
     }
+
+    const handleSpinPlusMark = () => {
+        if (spinVoucherId < spinVoucherCountData.length) {
+            const newId = spinVoucherId + 1;
+            const nextItem = spinVoucherCountData.find(item => item.id === newId);
+            if (nextItem) {
+                setSpinVoucherId(newId);
+                setSpinVoucherCount(nextItem.count);
+            }
+        }
+    }
+
+    const handleSpinMinusMark = () => {
+        if (spinVoucherId > 1) {
+            const newId = spinVoucherId - 1;
+            const prevItem = spinVoucherCountData.find(item => item.id === newId);
+            if (prevItem) {
+                setSpinVoucherId(newId);
+                setSpinVoucherCount(prevItem.count);
+            }
+        }
+    }
+
     // Mobile view based on the provided image
     if (isMobile) {
         return (
             <div className="flex flex-col w-full min-h-screen bg-[#1A202C] text-white">
-                <div className="absolute top-4 left-4 z-10">
+                <div className="absolute top-12 left-3 z-10">
                     <button
                         className="w-12 h-12 rounded-full flex items-center justify-center"
                         onClick={() => navigate('/')}
@@ -171,14 +227,49 @@ export function SpinPage() {
                 </div>
                 {/* Spin buttons */}
                 <div className="px-4 pb-24 space-y-2">
+                    {selectedBalanceType === 'ticket' && (
+                        <>
+                            <div className="flex font-inter items-center justify-between w-full py-3 px-2 rounded-[22px] bg-[#374151] text-white h-[80px]">
+                                <button
+                                    className="font-extrabold px-4 text-[30px] leading-none h-[64px] w-[64px] flex items-center justify-center rounded-[22px] bg-[#67768F]"
+                                    onClick={handleSpinMinusMark}
+                                >
+                                    -
+                                </button>
+
+                                <div className="flex justify-center items-center pr-3 overflow-hidden">
+                                    <img
+                                        src={IMAGES.voucher}
+                                        alt="voucher"
+                                        className="h-full max-h-[90px] w-auto object-contain"
+                                    />
+                                    <p className="font-bold text-2xl cursor-default text-center w-[60px]">
+                                        {Number.isInteger(spinVoucherCount)
+                                            ? spinVoucherCount
+                                            : spinVoucherCount.toFixed(2)}
+                                    </p>
+                                </div>
+
+                                <button
+                                    className="font-extrabold px-4 text-[30px] leading-none h-[64px] w-[64px] flex items-center justify-center rounded-[22px] bg-[#67768F]"
+                                    onClick={handleSpinPlusMark}
+                                >
+                                    +
+                                </button>
+                            </div>
+                        </>
+                    )}
+
+                    {selectedBalanceType === 'coin' &&
+                        <button
+                            className="w-full py-4 rounded-[22px] bg-[#374151] text-white font-bold text-xl"
+                            disabled={isSpinning || spinBalance < 3}
+                        >
+                            3 x Spin
+                        </button>
+                    }
                     <button
-                        className="w-full py-4 rounded-xl bg-[#374151] text-white font-bold text-xl"
-                        disabled={isSpinning || spinBalance < 3}
-                    >
-                        3 x Spin
-                    </button>
-                    <button
-                        className="w-full py-4 rounded-xl bg-[#2D7FF0] hover:bg-blue-600 text-white font-bold text-xl transition-colors disabled:opacity-50 disabled:cursor-not-allowed"
+                        className="w-full py-4 rounded-[22px] bg-[#2D7FF0] hover:bg-blue-600 text-white font-bold text-2xl font-inter transition-colors disabled:opacity-50 disabled:cursor-not-allowed"
                         onClick={spinWheel}
                         disabled={isSpinning || spinBalance <= 0}
                     >
@@ -196,24 +287,29 @@ export function SpinPage() {
             </div>
         )
     }
+
     // Desktop view - unchanged
     return (
         <div className="flex flex-col w-full min-h-screen bg-[#1F2937] text-white">
-            {/* Back button and status bar */}
-            <div className="relative p-4">
-                <div className="flex items-center absolute top-4 left-4">
-                    <button
-                        className="w-12 h-12 rounded-full flex items-center justify-center"
-                        onClick={() => navigate('/giveaway-entry')}
-                    >
-                        <img
-                            src="https://uploadthingy.s3.us-west-1.amazonaws.com/5dZY2vpVSVwYT3dUEHNYN5/back-icons.png"
-                            alt="Back"
-                            className="w-8 h-8"
-                        />
-                    </button>
-                </div>
+            {/* Back button */}
+            <div className="absolute top-12 left-2 z-10 lg:top-4 md:top-4 md:left-4 sm:top-4">
+                <button
+                    className="w-12 h-12 rounded-full flex items-center justify-center"
+                    onClick={() => navigate('/giveaway-entry')}
+                >
+                    <img
+                        src="https://uploadthingy.s3.us-west-1.amazonaws.com/5dZY2vpVSVwYT3dUEHNYN5/back-icons.png"
+                        alt="Back"
+                        className="w-8 h-8"
+                    />
+                </button>
             </div>
+
+            {/* Status Bar */}
+            <div className="">
+                <StatusBar  isMobile={isMobile} hideOnlineCount={true} />
+            </div>
+
             {/* Main content */}
             <div className="flex-1 flex flex-col items-center justify-center mb-32 px-4 py-8 relative">
                 {/* Spin wheel container */}
@@ -244,15 +340,53 @@ export function SpinPage() {
                 </div>
                 {/* Spin buttons */}
                 <div className="flex flex-col md:flex-row gap-4 mt-4 w-full max-w-2xl">
-                    <button
-                        className="w-full py-3 px-4 rounded-2xl bg-[#374151] text-white font-semibold text-3xl"
-                        disabled={isSpinning || spinBalance < 25}
-                        onClick={() =>
-                            console.log('25x Spin button clicked (not implemented)')
-                        }
-                    >
-                        25 x Spin
-                    </button>
+                    {selectedBalanceType === 'ticket' && (
+                        <>
+                            <div className="flex font-inter items-center justify-between w-full py-3 px-4 rounded-2xl bg-[#374151] text-white h-[80px]">
+                                <button
+                                    className="font-extrabold px-4 text-[30px] leading-none h-[64px] w-[64px] flex items-center justify-center rounded-2xl bg-[#67768F]"
+                                    onClick={handleSpinMinusMark}
+                                >
+                                    -
+                                </button>
+
+                                <div className="flex justify-center items-center pr-3 overflow-hidden">
+                                    <img
+                                        src={IMAGES.voucher}
+                                        alt="voucher"
+                                        className="h-full max-h-[90px] w-auto object-contain"
+                                    />
+                                    <p className="font-bold text-2xl cursor-default text-center w-[60px]">
+                                        {Number.isInteger(spinVoucherCount)
+                                        ? spinVoucherCount
+                                        : spinVoucherCount.toFixed(2)}
+                                    </p>
+                                </div>
+
+                                <button
+                                    className="font-extrabold px-4 text-[30px] leading-none h-[64px] w-[64px] flex items-center justify-center rounded-2xl bg-[#67768F]"
+                                    onClick={handleSpinPlusMark}
+                                >
+                                    +
+                                </button>
+                            </div>
+                        </>
+                    )}
+
+                    {selectedBalanceType === 'coin' && (
+                        <>
+                            <button
+                                className="w-full py-3 px-4 rounded-2xl bg-[#374151] text-white font-semibold text-3xl"
+                                disabled={isSpinning || spinBalance < 25}
+                                onClick={() =>
+                                    console.log('25x Spin button clicked (not implemented)')
+                                }
+                            >
+                                25 x Spin
+                            </button>
+                        </>
+                    )}
+
                     <button
                         className="w-full py-3 px-4 rounded-2xl bg-[#2D7FF0] hover:bg-blue-600 text-white font-semibold text-3xl transition-colors disabled:opacity-50 disabled:cursor-not-allowed"
                         onClick={spinWheel}
