9d02d32fc26f779dc5236bba0cf0a37ebc2d422f
LRLayan <rameshlayan4@gmail.com>
2025-08-04 13:12:30 +0530

    modify AuthenticatedWinModal

--- src/components/GameModals/AuthenticatedWinModal.tsx
+++ src/components/GameModals/AuthenticatedWinModal.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useGlobalContext } from '../../context/GlobalContext'
+import {IMAGES} from "../../constance/imagesLink.ts";
 type AuthenticatedWinModalProps = {
     isOpen: boolean
     onClose: () => void
@@ -12,26 +13,55 @@ export function AuthenticatedWinModal({
                                           reward,
                                       }: AuthenticatedWinModalProps) {
     const navigate = useNavigate()
-    const { addCoins } = useGlobalContext()
+    const { addCoins, temporaryVoucherBalance, temporaryTicketBalance, temporaryCoinBalance, setVoucherBalance , setTicketBalance, voucherBalance, ticketBalance, selectedBalanceType } = useGlobalContext()
+
     if (!isOpen) return null
+
     const handleCollect = () => {
-        addCoins(reward)
+        if (selectedBalanceType === 'coin') {
+            addCoins(reward)
+        } else {
+            addCoins(temporaryCoinBalance)
+            setVoucherBalance(voucherBalance + temporaryVoucherBalance);
+            setTicketBalance(ticketBalance - temporaryTicketBalance);
+        }
+
         onClose()
         navigate('/')
     }
+
     return (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-[#1F2937]">
             <div className="bg-[#374151] rounded-2xl p-6 text-center text-white w-[320px] flex flex-col items-center">
-                <h2 className="text-3xl text-[#FFFFFF] font-semibold mt-10 mb-24 font-[Inter]">Congratulations !</h2>
-                <p className="text-xl font-medium mb-10 font-[Inter] text-[#FFFFFF]">You Win</p>
-                <div className="flex items-center justify-center text-5xl font-bold mb-36">
+                <h2 className={`${selectedBalanceType !== 'ticket' ? 'mb-24' : 'mb-10'} text-3xl text-[#FFFFFF] font-semibold mt-10 font-[Inter]`}>Congratulations !</h2>
+                <p className="text-[18px] font-medium mb-10 text-[#FFFFFF]">You Win</p>
+                <div className={`${selectedBalanceType !== 'ticket' && 'mb-36' } flex items-center justify-center text-5xl font-bold`}>
                     <img
                         src="https://uploadthingy.s3.us-west-1.amazonaws.com/fmLBFTLqfqxtLWG949C3wH/point.png"
                         alt="Coins"
                         className="w-6 h-6 mr-4"
                     />
-                    <span className="text-2xl font-medium font-[Inter] text-[#FFFFFF]">{reward.toLocaleString()}</span>
+                    { selectedBalanceType === 'ticket' ?
+                        <span className="text-2xl font-medium font-[Inter] text-[#FFFFFF]">{temporaryCoinBalance.toLocaleString()}</span>
+                        :
+                        <span className="text-2xl font-medium font-[Inter] text-[#FFFFFF]">{reward.toLocaleString()}</span>
+                    }
                 </div>
+
+                { selectedBalanceType === 'ticket' &&
+                    <div className={"font-inter mb-20"}>
+                        <h2 className={"mt-5"}>+</h2>
+                        <div className={"flex items-center"}>
+                            <img
+                                src={IMAGES.voucher}
+                                alt="Coins"
+                                className="w-12 h-16"
+                            />
+                            <span className="text-2xl font-medium text-[#FFFFFF]">x {temporaryVoucherBalance.toLocaleString()} free</span>
+                        </div>
+                    </div>
+                }
+
                 <button
                     className=" w-48 bg-[#4E80F1] hover:bg-blue-600 text-[#FFFFFF] font-semibold py-3 px-8 rounded-xl text-xl font-[Inter] "
                     onClick={handleCollect}
