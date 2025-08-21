08b37dd3cadfbe9e36f036fdf79eeea0a7446558
likshan-lahiru <lahiru212001@gmail.com>
2025-08-21 22:54:58 +0530

    refactor: enhance FlipCard and FlipPage components with improved card data structure and mobile responsiveness

--- src/constance/imagesLink.ts
+++ src/constance/imagesLink.ts
@@ -1,12 +1,19 @@
 export const IMAGES = {
     // voucher: 'https://uploadthingy.s3.us-west-1.amazonaws.com/uwPYNNRiavmZZ285SkzD5Z/diaomnd.png',
     voucher: '/ti 59.png',
-    diamond: 'https://uploadthingy.s3.us-west-1.amazonaws.com/agrcZVSRX593jbti3xzVTM/heart.png',
+    diamond:
+        'https://uploadthingy.s3.us-west-1.amazonaws.com/agrcZVSRX593jbti3xzVTM/heart.png',
     coin: 'https://uploadthingy.s3.us-west-1.amazonaws.com/2XiBYwBWgNJxytH6Z2jPWP/point.png',
-    ticket: 'https://uploadthingy.s3.us-west-1.amazonaws.com/65WCbcmf6dyyeqvjSAJHyp/fire.png',
-    cookyFlip: '/CookyFlip.png',
+    ticket:
+        'https://uploadthingy.s3.us-west-1.amazonaws.com/65WCbcmf6dyyeqvjSAJHyp/fire.png',
+    cookyFlip: 'https://uploadthingy.s3.us-west-1.amazonaws.com/jS53VZTAEiCmJuW3z3gbXA/cookyflip.png',
     logo: 'https://uploadthingy.s3.us-west-1.amazonaws.com/uYsqLmRkx6f1zBGRqMZA6G/cookycreanlogo3.png',
-    fortuneCooky: '/fortuneCooky.png',
-    badCooky: '/badCooky.png',
-    freeFlip: '/freeFlip.png'
-};
+    fortuneCooky:
+        'https://uploadthingy.s3.us-west-1.amazonaws.com/jS53VZTAEiCmJuW3z3gbXA/cookyflip.png',
+    badCooky: 'https://i.ibb.co/9HySDjS7/bad-cooky.png',
+    freeFlip: 'https://i.ibb.co/SXhHWHTn/free-jar.png',
+    outOfStock: 'https://i.ibb.co/0pd8DxL0/outofstock.png',
+    crackedCooky :'https://i.ibb.co/BHNs6mMw/crakc.png',
+    antsInTheJar: 'https://i.ibb.co/8gJvnV78/ant.png'
+}
+//https://i.ibb.co/k2LgHH1m/grandwin.png
